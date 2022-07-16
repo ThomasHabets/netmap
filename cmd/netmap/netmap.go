b49af7a0558850da1f018f0716548be7356dcb76
Thomas Habets <thomas@habets.se>
2022-07-16 08:19:55 +0100

    Add map naming, and gofmt

--- cmd/netmap/netmap.go
+++ cmd/netmap/netmap.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"net/http"
-	"sort"
-	"encoding/json"
-	"fmt"
-	"context"
-	"io/ioutil"
-	"strings"
 	"bytes"
-	"embed"
-	"flag"
+	"context"
 	"database/sql"
+	"embed"
+	"encoding/json"
+	"flag"
+	"fmt"
 	"html/template"
-	texttemplate "text/template"
+	"io/ioutil"
+	"net/http"
 	"os/exec"
+	"sort"
+	"strings"
+	texttemplate "text/template"
 
-	log "github.com/sirupsen/logrus"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	// TODO
+	mapName = "main"
 )
 
 var (
@@ -28,13 +33,13 @@ var (
 	//go:embed graph.dot
 	graphTmplString string
 
-	rootTmpl = template.Must(template.New("root").Parse(rootTmplString))
+	rootTmpl  = template.Must(template.New("root").Parse(rootTmplString))
 	graphTmpl = texttemplate.Must(texttemplate.New("graph").Parse(graphTmplString))
 
 	//go:embed static/netmap.css
 	//go:embed static/netmap.js
 	fs embed.FS
-	
+
 	db *sql.DB
 )
 
@@ -52,52 +57,61 @@ func rootHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 type Router struct {
-	ID string
+	ID   string
 	Name string
-	Pos string
+	Pos  string
 }
 
 type Net struct {
-	ID string
+	ID  string
 	Pos string
 }
 
 type Link struct {
 	Router string
-	Net string
+	Net    string
+	Cost   int
 }
 
 type graphData struct {
 	Router []Router
-	Net []Net
-	Link []Link
+	Net    []Net
+	Link   []Link
 }
-
 
 func getPositions(ctx context.Context) (map[string]string, error) {
 	ret := make(map[string]string)
-	rows, err := db.QueryContext(ctx, `SELECT id,x,y FROM pos ORDER BY id`)
+	rows, err := db.QueryContext(ctx, `
+SELECT node_id,x,y
+FROM pos
+NATURAL JOIN maps
+WHERE maps.name=?
+ORDER BY node_id
+`, mapName)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var k string
-		var x,y int
+		var x, y int
 		if err := rows.Scan(&k, &x, &y); err != nil {
 			return nil, err
 		}
-		ret[k] = fmt.Sprintf("%d,%d", x,y)
+		ret[k] = fmt.Sprintf("%d,%d", x, y)
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
-	return ret,nil
+	return ret, nil
 }
 
 func getNames(ctx context.Context) (map[string]string, error) {
 	ret := make(map[string]string)
-	rows, err := db.QueryContext(ctx, `SELECT node_id,name FROM nodenames ORDER BY node_id`)
+	rows, err := db.QueryContext(ctx, `
+SELECT node_id,name
+FROM nodenames
+ORDER BY node_id`)
 	if err != nil {
 		return nil, err
 	}
@@ -113,7 +127,7 @@ func getNames(ctx context.Context) (map[string]string, error) {
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
-	return ret,nil
+	return ret, nil
 }
 
 func generateGraphData(ctx context.Context) (*graphData, error) {
@@ -126,11 +140,11 @@ func generateGraphData(ctx context.Context) (*graphData, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	var graph graphData
-	rows, err := db.QueryContext(ctx, "SELECT router, net, cost FROM links")
+	rows, err := db.QueryContext(ctx, `SELECT router, net, cost FROM links`)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	defer rows.Close()
 	seen := make(map[string]bool)
@@ -141,16 +155,16 @@ func generateGraphData(ctx context.Context) (*graphData, error) {
 			return nil, err
 		}
 		if !seen[router] {
-			e:=Router{
-				ID: router,
+			e := Router{
+				ID:   router,
 				Name: names[router],
-				Pos: poss[router],
+				Pos:  poss[router],
 			}
-			graph.Router =append(graph.Router, e)
+			graph.Router = append(graph.Router, e)
 		}
 		if !seen[link] {
-			e:=Net{
-				ID: link,
+			e := Net{
+				ID:  link,
 				Pos: poss[link],
 			}
 			graph.Net = append(graph.Net, e)
@@ -171,10 +185,9 @@ func generateGraphData(ctx context.Context) (*graphData, error) {
 	})
 	return &graph, nil
 }
-	
 
 func generateDot(ctx context.Context) (string, error) {
-	graph, err:=generateGraphData(ctx)
+	graph, err := generateGraphData(ctx)
 	if err != nil {
 		return "", err
 	}
@@ -200,16 +213,19 @@ func updateHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	var u update
-	if err := json.Unmarshal(b, &u);err!=nil {
+	if err := json.Unmarshal(b, &u); err != nil {
 		log.Error(err)
 		http.Error(w, "Failed to parse request JSON", http.StatusBadRequest)
 		return
 	}
-	if res, err := db.ExecContext(req.Context(), `UPDATE pos SET x=?,y=? WHERE id=?`,u.X,u.Y,id);err!= nil {
+	if res, err := db.ExecContext(req.Context(), `
+UPDATE pos SET x=?,y=?
+WHERE node_id=?
+AND map_id=(SELECT map_id FROM maps WHERE name=?)`, u.X, u.Y, id, mapName); err != nil {
 		log.Error(err)
 		http.Error(w, "Failed to update", http.StatusInternalServerError)
 		return
-	} else if n, err:=res.RowsAffected(); err  != nil {
+	} else if n, err := res.RowsAffected(); err != nil {
 		log.Warningf("Failed to get rows affected for %q", id)
 		// Pretend to caller that it succeeded.
 	} else if n != 1 {
@@ -241,13 +257,13 @@ func renderHandler(w http.ResponseWriter, req *http.Request) {
 			break
 		}
 	}
-	
+
 	var stderr bytes.Buffer
-	cmd:=exec.CommandContext(req.Context(), "dot", "-T" + format)
+	cmd := exec.CommandContext(req.Context(), "dot", "-T"+format)
 	cmd.Stdout = w
 	cmd.Stdin = bytes.NewBufferString(dot)
 	cmd.Stderr = &stderr
-	if err := cmd.Run(); err != nil{
+	if err := cmd.Run(); err != nil {
 		log.Errorf("graphviz: %v: %s\n%s", err, stderr.String(), dot)
 		return
 	}
